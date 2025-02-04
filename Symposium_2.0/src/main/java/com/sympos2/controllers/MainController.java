ad6251c7a9dfa0b578ea751dc8ef98d251923505
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-04 21:20:45 +0100

    Edited the MainController
    Added Get and PostMapping for the registrer template

--- Symposium_2.0/src/main/java/com/sympos2/controllers/MainController.java
+++ Symposium_2.0/src/main/java/com/sympos2/controllers/MainController.java
@@ -5,9 +5,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.sympos2.models.Usuario;
 import com.sympos2.repositories.UserRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class MainController {
@@ -40,4 +47,45 @@ public class MainController {
 		model.addAttribute("listaUsuarios", UserRepo.findAll());
 		return "admin-zone-users-list";
 	}
+	
+	@GetMapping("register")
+	public String registro(Model model) {
+		// Here goes the things before the page charges
+		return "/register";
+	}
+	
+	@PostMapping("/submit")
+	public String handleFormSubmit(@Valid @ModelAttribute("userForm") Usuario nuevoUsuario, BindingResult br, RedirectAttributes redirectAttributes) {
+		String retorno="";
+		String mensaje="";
+		boolean check=true;
+		
+		if (br.hasErrors()) {
+			check=false;
+			mensaje+="Errors in register\n";
+		} 
+		if (nuevoUsuario.getName()==null) {
+			check=false;
+			mensaje+="You needs an username";
+		}
+		if (nuevoUsuario.getFechaNac()==null) {
+			check=false;
+			mensaje+="You needs a birthday date\n";
+		}
+		if (nuevoUsuario.getEmail()==null || !nuevoUsuario.getEmail().contains("@")) {
+			check=false;
+			mensaje+="You needs a valid email\n";
+		}
+		
+		if (check==false) {
+			redirectAttributes.addFlashAttribute("message", mensaje);  // Usar addFlashAttribute
+			return "redirect:/register";  // Redirigir a la página de registro
+		} else {
+			nuevoUsuario.setRole("STUDENT");
+			UserRepo.save(nuevoUsuario);
+			retorno="index";
+		}
+		return retorno;
+	}
+	
 }
