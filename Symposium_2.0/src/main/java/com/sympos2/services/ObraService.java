d03b78d4220b07828da8e7233a3a9abbb29a1d7f
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-09 20:24:45 +0100

    ObraService class edited
    Added a method to list alll the different themes in the works saved in the database

--- Symposium_2.0/src/main/java/com/sympos2/services/ObraService.java
+++ Symposium_2.0/src/main/java/com/sympos2/services/ObraService.java
@@ -1,13 +1,35 @@
 package com.sympos2.services;
 
+import java.text.Normalizer;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
+import com.sympos2.models.Obra;
+import com.sympos2.repositories.ObraRepository;
+
 @Service
-public class ObraService {
+public class ObraService{
 	
 	@Autowired 
 	private MongoTemplate mt;
 	
+	@Autowired
+	private ObraRepository obraRepo;
+	
+	public List<String> findAllDifferentTemas() {
+        List<String> temas = mt.query(Obra.class)
+                            .distinct("temas")
+                            .as(String.class)
+                            .all();
+        return temas.stream()
+                .map(tema -> Normalizer.normalize(tema, Normalizer.Form.NFD)) // Eliminar acentos
+                .map(tema -> tema.replaceAll("\\p{InCombiningDiacriticalMarks}+", "")) // Quitar diacríticos
+                .map(String::toLowerCase) // Convertir a minúsculas
+                .distinct() // Evitar duplicados después de normalizar
+                .collect(Collectors.toList());
+    }
 }
