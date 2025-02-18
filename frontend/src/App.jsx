1b639a4f3e94b27ac7506fb8971bc11397987c8d
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-18 22:36:47 +0100

    Creation of Frontend React Project
    Created the project and edited the vite.config.js document to read the URL through the .env document that was created too to encrypt the API URL

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+
+function App() {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <h2>Hola mundo!</h2>
+    </>
+  )
+}
+
+export default App
