e22aefa98fd4eacdde0d205ec1d1a10a5d631020
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-18 22:54:46 +0100

    AdminTool Component created
    Created a component to show the admin pages only to admin users and its css document

---
+++ frontend/src/components/AdminToolComponent.jsx
@@ -0,0 +1,38 @@
+import './AdminToolComponent.css'
+import React, { useState } from 'react';
+
+function AdminToolComponent() {
+    // Declaramos el estado isMenuOpen para controlar la visibilidad del menú
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Función para alternar la visibilidad del desplegable
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !prevState);
+    };
+
+    return (
+        <div>
+            {/* Imagen que activa el desplegable */}
+            <img 
+                id="devToolImg" 
+                src='/destornillador.png' 
+                alt="Herramientas de desarrollo" 
+                onClick={toggleMenu} // Aquí se usa onClick y se pasa la función directamente
+                className={isMenuOpen ? 'open' : 'closed'} 
+            />
+
+            {/* Menú desplegable */}
+            {isMenuOpen && (
+                <div id="devToolDropdown">
+                    <ul  className={isMenuOpen ? 'open' : 'closed'}>
+                        <li><a href="/usersList">Users List</a></li>
+                        <li><a href="/workList">Writings List</a></li>
+                        <li><a href="/commentList">Comments List</a></li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default AdminToolComponent;
