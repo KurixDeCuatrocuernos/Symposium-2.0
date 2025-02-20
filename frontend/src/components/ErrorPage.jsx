bd63dff405fc436e2262cdfd2f9a507891589c21
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-20 21:51:54 +0100

    Created ErrorPage in frontend

---
+++ frontend/src/components/ErrorPage.jsx
@@ -0,0 +1,12 @@
+
+
+
+function ErrorPage(){
+
+    return(
+        <div>
+            <h1>Estás en la página de error: Página no encontrada</h1>
+        </div>
+    );
+}
+export default ErrorPage;
