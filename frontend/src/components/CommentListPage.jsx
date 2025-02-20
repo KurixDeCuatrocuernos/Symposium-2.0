13714ec4b2e19f68daeec21e4b5e15a4a0b49186
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-20 21:49:03 +0100

    Created CommentListPage in frontend

---
+++ frontend/src/components/CommentListPage.jsx
@@ -0,0 +1,12 @@
+import "./CommentListPage.css";
+
+function CommentListPage(){
+
+    return(
+        <div>
+            <h1>Estás en la página para listar comentarios</h1>
+        </div>
+    );
+}
+
+export default CommentListPage;
