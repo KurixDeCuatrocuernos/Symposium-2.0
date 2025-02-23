d3463a63427b3ac7ae660bd3b2459763c3964e9d
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-23 23:23:50 +0100

    UsuarioComentarioPintado class edited
    Added school parameter to show in the front

--- Symposium_2.0/src/main/java/com/sympos2/dto/UsuarioComentarioPintado.java
+++ Symposium_2.0/src/main/java/com/sympos2/dto/UsuarioComentarioPintado.java
@@ -1,5 +1,5 @@
 package com.sympos2.dto;
 
-public record UsuarioComentarioPintado (String id, String name, String role, String avatar){
+public record UsuarioComentarioPintado (String id, String name, String role, String avatar, String school){
 
 }
