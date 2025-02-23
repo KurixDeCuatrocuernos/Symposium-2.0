b2a4c596f5534127d41bab2b90e3b0fd2f3e6db8
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-23 23:53:21 +0100

    SignInMessageComponent created
    Created a component to redirect the user to LoginPage if he/she wish when tries to comment without be logged

---
+++ frontend/src/components/SignInMessageComponent.jsx
@@ -0,0 +1,23 @@
+import "./SignInMessageComponent.css";
+import React from 'react';
+
+const SignInMessageComponent = ({ onClose }) => {
+
+    const logInRedir = () =>{
+        window.location.href="/login";
+    };
+
+    return (
+        <div className="overlay">
+            <div className="modal">
+                <h2>You need to log in!</h2>
+                <button onClick={logInRedir}>Log In</button>
+                <button onClick={onClose}>Cancel</button>
+                <br/>
+                <a href="/registry">Don't have account, register now here!</a>
+            </div>
+        </div>
+    );
+};
+
+export default SignInMessageComponent;
