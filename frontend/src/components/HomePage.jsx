32e90680a2a0fe9103721043fd4b9f25acac8b33
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-02-27 23:12:56 +0100

    HomePage and Header edited
    Repared an error in HeaderPage and prepared the HomePage to create it.

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -3,9 +3,9 @@ import WritingComponent from "./WritingComponent";
 function HomePage(){
 
     return(
-        <div>
-            <h1>Estás viendo la página de inicio</h1>
-        </div>
+        <>
+        
+        </>
     );
 
 }
