c074381d695d721f46e44944573e15e5fa474695
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-03-01 22:51:20 +0100

    ComentarioComponent edited
    Added the method to list answers of a Comment

--- frontend/src/components/ComentarioComponent.jsx
+++ frontend/src/components/ComentarioComponent.jsx
@@ -1,9 +1,84 @@
 import "./ComentarioComponent.css";
 import React, { useState, useEffect } from "react";
+import CommentInsertComponent from "./CommentInsertComponent";
+import AnswerComponent from "./AnswerComponent";
+import SignInMessageComponent from "./SignInMessageComponent";
 
-function ComentarioComponent({ username, school, value, title, text, datetime }){
+function ComentarioComponent({ id, username, school, value, title, text, datetime, usr, isbn, role }){
 
+    const [answer, setAnswer] = useState(false);
+    const [answerList, setAnswerList] = useState([]);
+    const [signInMessage, setSignInMessage] = useState(false);
+    const [logged, setLogged] = useState(false);
+
+    useEffect(() => {
+        getAnswers();
+    }, []);
+
+    const banComment = async() => {
+        
+        let cell = confirm("¿Are you sure you want to ban the comment with id: "+id+"?");
+        if (cell === true){
+            try{
+                const response = await fetch('/getBanComment?id='+id);
+                if (response.ok){
+                    const data = await response.json();
+                    if (data.status==="true"){
+                        window.location.reload();
+                    } else {
+                        cons("There was a problem in server: "+data.message);
+                    }
+                } else {
+                    console.log("response is not ok!");
+                }
+            } catch (error){
+                alert("There was a problem connecting with the server: "+error);
+            }
+        } 
+        
+    };
+
+    const getAnswers = async() => {
+        try{
+            const response = await fetch('/getAnswers?id='+isbn+'&comment='+id);
+            if (response.ok){
+                const data = await response.json();
+                if (data.status==="true"){
+                    setAnswerList(data.array);
+                } else {
+                    cons("There was a problem in server: "+data.message);
+                }
+            } else {
+                console.log("response is not ok!");
+            }
+        } catch (error){
+            alert("There was a problem connecting with the server: "+error);
+        }
+    };
+
+    
+    const closeAnswer = () => {
+        setAnswer(false);
+    };
+
+
+    const handleAnswer = () => {
+        setAnswer(true);
+    };
+
+    const handleLoginMessage = () => {
+        setSignInMessage(true);
+    };
+
+    const closeLoginMessage = () => {
+        setSignInMessage(false);
+    };
+    
+    
     return(
+        <>
+        {answer && <CommentInsertComponent onClose={closeAnswer} id={id} answer={answer} usr={usr} isbn={isbn}/>}
+        {signInMessage && <SignInMessageComponent onClose={closeLoginMessage}/>}
         <div id="CommentContainer">
             <div id="userContent">
                 <img id="userImg" src="usuario.png"></img>
@@ -19,9 +94,31 @@ function ComentarioComponent({ username, school, value, title, text, datetime })
                     <strong id="commentTitle">{title}</strong>
                     <p id="commentText">{text}</p>
                     <p id="commentDate">{datetime}</p>
+                    { usr !== null && role!=="ADMIN" ? <img onClick={handleAnswer} src="pregunta-y-respuesta.png"></img> : '' }
+                    { usr === null ? <img onClick={handleLoginMessage} src="pregunta-y-respuesta.png"></img> : '' }
+                    {role==="ADMIN" ? <img onClick={banComment} src="prohibido.png"></img> : '' }
+                    
                 </div>
             </div>
         </div>
+        {answerList.map((answer) => {
+            if (answer.comentario === id) {
+                return (
+                    <AnswerComponent 
+                        key={answer.id}
+                        username={answer.username} 
+                        school={answer.school} 
+                        role={answer.role} 
+                        title={answer.title} 
+                        text={answer.text} 
+                        datetime={answer.datetime} 
+                        id={answer.id}
+                    />
+                );
+            }
+            return null; // Si no coincide, no devolver nada
+        })}
+        </>
     );
 
 }
