2c0022ee20d28b20705bfada2b5738dfd71c0905
KurixDeCuatrocuernos <alejandroma56789@gmail.com>
2025-03-01 22:50:23 +0100

    CommentListPage edited
    Finished the page to show banned comments

--- frontend/src/components/CommentListPage.jsx
+++ frontend/src/components/CommentListPage.jsx
@@ -1,11 +1,92 @@
 import "./CommentListPage.css";
+import { useState, useEffect } from "react";
+import AnswerComponent from "./AnswerComponent";
 
-function CommentListPage(){
+function CommentListPage() {
+    const [commentsList, setCommentsList] = useState([]);
+    const [emptyMessage, setEmptyMessage] = useState("");
+    const [commentDelList, setCommentDelList] = useState(true);
 
-    return(
-        <div>
-            <h1>Estás en la página para listar comentarios</h1>
-        </div>
+    useEffect(() => {
+        getAllBanComments();
+    }, []);
+
+    const getAllBanComments = async () => {
+        try {
+            const response = await fetch('/getAllBanComments');
+            if (response.ok) {
+                const data = await response.json();
+                if (data.status === "true") {
+                    if (data.array === "false") {
+                        setEmptyMessage("There are no banned comments yet");
+                        console.log(data);
+                    } else {
+                        setCommentsList(data.array);
+                    }
+                } else {
+                    alert(data.message);
+                }
+            } else {
+                console.log("response not ok");
+            }
+        } catch (error) {
+            console.log("error connecting with server to search: " + error);
+        }
+    };
+
+    const searchComments = async (event) => {
+        const search = event.target.value;
+        if (search === "") {
+            getAllBanComments();
+        } else {
+            try {
+                const response = await fetch('/getSearchCommentList?search=' + search);
+                if (response.ok) {
+                    const data = await response.json();
+                    if (data.status === "true") {
+                        setCommentsList(data.array);
+                    } else {
+                        alert(data.message);
+                    }
+                } else {
+                    console.log("response not ok");
+                }
+            } catch (error) {
+                console.log("error connecting with server to search: " + error);
+            }
+        }
+    };
+
+    return (
+        <>
+            <div id="overlay-CommentListPage">
+                <h1>Writings List</h1>
+                <div id="searchContainer-WorkList">
+                    <p>Search: </p>
+                    <input type="search" onChange={searchComments}></input>
+                    <img src="lupa.png" alt="Search" />
+                </div>
+                <div id="commentsContainer-CommentListPage">
+                    {emptyMessage && <h1>{emptyMessage}</h1>}
+                    {commentsList.length > 0 ? (
+                        commentsList.map((comment) => (
+                            <AnswerComponent
+                                key={comment.comment.id}
+                                id={comment.comment.id}
+                                username={comment.username}
+                                role={comment.userrole}
+                                title={comment.comment.titulo}
+                                text={comment.comment.texto}
+                                datetime={comment.comment.fecha}
+                                admin={commentDelList}
+                            />
+                        ))
+                    ) : (
+                        <p>No comments available</p>
+                    )}
+                </div>
+            </div>
+        </>
     );
 }
 
